63880f72b2df58f75f6d258d472780e8ebd7884c
Franklyn Rodriguez <franklynrodriguez.swe@gmail.com>
2023-11-06 10:21:48 -0500

    Addeding data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from 'react';
 import Search from './components/Search';
 import PhotoList from './components/PhotoList';
 import Nav from './components/Nav';
+import PhotoNotFound from "./components/PhotoNotFound";
+import apiKey from "./config";
+// import axios from 'axios';
+
+
+
 
 function App() {
+  const [pics, setPics] = useState([]);
+  useEffect(() => {
+   
+    fetch(`https://api.flickr.com/services/rest/?api_key=${apiKey}`)
+    .then((response) => response.json())
+    .then((responseData) => setPics(responseData.data))
+    .catch(error => console.log("Error fetching and parsing data", error));
+  }, []);
   return (
     <div>
       <Search />
-      <PhotoList />
       <Nav />
+      {/* <Routes>
+      <Route path="/" element={<LandingPage/>} />
+      <Route path="/cats" element={<LandingPage/>} />
+      <Route path="/dogs" element={<LandingPage/>} />
+      <Route path="/computers" element={<LandingPage/>} />
+      <Route path="/search/:query" element={<FourZeroFour/>} />
+      <Route path="/*" element={<PhotoNotFound/>} />
+      </Routes> */}
+      <PhotoList />
+
     </div>
   )
 }
